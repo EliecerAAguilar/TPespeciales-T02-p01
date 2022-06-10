3a54c448527134490e7a804e06ac051c03a80d31
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-06-10 15:18:44 -0500

    correcion de errores y añadiendo valores a la tabla track

--- modifica_chinook.py
+++ modifica_chinook.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import pandas as pd
+import traceback as t
 
 
 def cleaning_list(lista):
@@ -37,7 +38,7 @@ def main():
         """conexion a la base de datos"""
         chinook_db = sqlite3.connect("sql_db/chinook.db")
         cursor = chinook_db.cursor()
-
+        cursor.execute("""BEGIN""")
         """query CREAR  la tabla de subgeneros en la db"""
         cursor.execute("""CREATE TABLE IF NOT EXISTS subgenres (
                         subgenreid INTEGER PRIMARY KEY NOT NULL,
@@ -49,17 +50,18 @@ def main():
         """insertando los valores solicitados dependiendo de si tiene o no un nombre alternativo"""
         for x in range(len(subgen_list)):
             if len(subgen_list[x]) < 2:
-                cursor.execute("""INSERT INTO subgenres(subgenrename,subgenrealtname,genreid) VALUES(
-                                :subgenrename,
-                                NULL,
-                                (SELECT GenreId FROM genres WHERE Name LIKE '%punk%')
-                               )""",
+                cursor.execute("""INSERT OR REPLACE INTO subgenres(subgenrename,subgenrealtname,genreid) 
+                                VALUES(
+                                        :subgenrename,
+                                        NULL,
+                                        (SELECT GenreId FROM genres WHERE Name LIKE '%punk%')
+                                       )""",
                                {
                                    'subgenrename': subgen_list[x][0]
                                }
                                )
             else:
-                cursor.execute("""INSERT INTO subgenres(subgenrename,subgenrealtname,genreid) 
+                cursor.execute("""INSERT OR REPLACE INTO subgenres(subgenrename,subgenrealtname,genreid) 
                             VALUES(
                                     :subgenrename,
                                     :subgenrealtname,
@@ -75,33 +77,35 @@ def main():
         chinook_db.commit()
 
         """agregar una nueva columna a la tabla tracks """
-        # cursor.execute("""ALTER TABLE tracks ADD subgenere VARCHAR(150)""")
-        # chinook_db.commit()
+        cursor.execute("""ALTER TABLE tracks ADD subgenere VARCHAR(150)""")
+        chinook_db.commit()
 
         """generar 5 nuevas canciones en la tablas tracks"""
         artist_name = "Joe Lally"
         cursor.execute("""INSERT INTO artists(Name)
-                        VALUES(?)""",
-                       artist_name
+                        VALUES(:Name)""",
+                       {'Name': artist_name}
                        )
-        cursor.execute("SELECT last_insert_rowid()")
+        cursor.execute("""SELECT last_insert_rowid()""")
         artist_id = cursor.fetchone()
+
         chinook_db.commit()
 
         album_title = "13 Songs"
-        cursor.execute("""INSERT INTO albums(Title)
-                        VALUES(?)""",
-                       album_title)
-        cursor.execute("SELECT last_insert_rowid()")
-        albun_id = cursor.fetchone()
+        cursor.execute("""INSERT INTO albums(Title,ArtistId)
+                        VALUES(:Title, :ArtistId)""",
+                       {'Title' : album_title,'ArtistId': artist_id[0]})
+        cursor.execute("""SELECT last_insert_rowid()""")
+        album_id = cursor.fetchone()
         chinook_db.commit()
 
         subgenre_name = "punk rock"
-        cursor.execute("""INSERT INTO subgenres(subgrenrename)
-                                VALUES(?)""",
-                       subgenre_name
+        cursor.execute("""INSERT INTO subgenres(subgenrename,subgenrealtname,genreid)
+                                VALUES(:subgenrename,NULL,4)""",
+                       {'subgenrename' : subgenre_name}
                        )
-        cursor.execute("SELECT last_insert_rowid()")
+
+        cursor.execute("""SELECT last_insert_rowid()""")
         subgenre_id = cursor.fetchone()
         chinook_db.commit()
 
@@ -122,24 +126,28 @@ def main():
 
         """https://music.apple.com/us/album/waiting-room/49249845?i=49249816"""
         data = [
-                    ("Waiting Room", albun_id, 1, 4, "Ted Niceley", 2530, 47000000, 5.99, subgenre_id),
-                    ("Bulldog Front", albun_id, 1, 4, "Ted Niceley", 2530, 47000000, 5.99, subgenre_id),
-                    ("Bad Mouth", albun_id, 1, 4, "Ted Niceley", 2350, 28000000, 5.99, subgenre_id),
-                    ("Burning", albun_id, 1, 4, "Ted Niceley", 2390, 30000000, 5.99, subgenre_id),
-                    ("Give Me the Cure", albun_id, 1, 4, "Ted Niceley", 2580, 47000000, 5.99, subgenre_id),
+                    ("Waiting Room", album_id[0], 1, 4, "Ted Niceley", 2530, 47000000, 5.99, subgenre_id[0]),
+                    ("Bulldog Front", album_id[0], 1, 4, "Ted Niceley", 2530, 47000000, 5.99, subgenre_id[0]),
+                    ("Bad Mouth", album_id[0], 1, 4, "Ted Niceley", 2350, 28000000, 5.99, subgenre_id[0]),
+                    ("Burning", album_id[0], 1, 4, "Ted Niceley", 2390, 30000000, 5.99, subgenre_id[0]),
+                    ("Give Me the Cure", album_id[0], 1, 4, "Ted Niceley", 2580, 47000000, 5.99, subgenre_id[0]),
                 ]
-        cursor.execute("""INSERT INTO tracks( Name, AlbumId, MediaTypeId,GenreId,Composer,Milliseconds,Bytes,UnitPrice,subgenere)
-                VALUES(?, ?, ?,?,?,?,?,?,?)""", data)
-
+        cursor.executemany("""INSERT INTO tracks(Name, AlbumId, MediaTypeId,GenreId,Composer,Milliseconds,Bytes,UnitPrice,subgenere)
+            VALUES(?, ?, ?,?,?,?,?,?,?)""", data)
+        chinook_db.commit()
         """cerrar conexion a la base de datos"""
         chinook_db.close()
 
     except FileNotFoundError:
         print("archivo no encontrado")
+        print(t.format_exc())
     except sqlite3.Error as sql_e:
         print("SQLite3 Error\n", sql_e)
+        print(t.format_exc())
     except sqlite3.OperationalError as sql_op_e:
         print("Operational Error\n", sql_op_e)
+        print(t.format_exc())
+        cursor.execute("""ROLLBACK""")
 
 
 if __name__ == "__main__":
